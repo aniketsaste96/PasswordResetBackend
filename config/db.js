70aaa3aacd13f8e4b74a14915c9be31576324b80
Aniket <aniketsaste96@gmail.com>
2022-02-15 16:20:43 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 //always use async fun for db
 // add uri in config.env
+const MONGO_URI = "mongodb+srv://aks:aks123@cluster0.f01yf.mongodb.net/resetPassword?retryWrites=true&w=majority"
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(MONGO_URI)
     //useNewUrlParser, useUnifiedTopology, useFindAndModify, and useCreateIndex are no longer supported options. 
 
     console.log("MONGODB CONNECTED!!!")
