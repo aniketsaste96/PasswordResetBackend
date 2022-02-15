052094cf76ff6749ac19a7078a06d7813afe9ad9
Aniket <aniketsaste96@gmail.com>
2022-02-15 16:15:23 +0530

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(errorHandler);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => console.log(`server running on ${PORT}`));
 
